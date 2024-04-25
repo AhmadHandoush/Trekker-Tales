de81ac0809b2bd8b1f6943521cf58e849385f167
hamada <hamadahandoush9991@gmail.com>
2024-04-25 09:17:14 +0200

    modify posts table

--- server/app/Http/Controllers/PostController.php
+++ server/app/Http/Controllers/PostController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function create_post($req){
-        
+    public function create_post($request){
+        $request->validate([
+            'name' => 'required|string',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+
 
     }
 }
