47d39dcd9a7a60814845813435b233ac33853327
hamada <hamadahandoush9991@gmail.com>
2024-04-30 14:58:46 +0200

    Admin dashboard: design the card

--- admin-dashboard/src/pages/main/components/TopCard.jsx
+++ admin-dashboard/src/pages/main/components/TopCard.jsx
@@ -2,13 +2,12 @@ function TopCard() {
   return (
     <div className="top-card">
       <img
-        src="../public/360_F_113467839_JA7ZqfYTcIFQWAkwMf3mVmhqXr7ZOgEX.jpg"
+        src="./360_F_113467839_JA7ZqfYTcIFQWAkwMf3mVmhqXr7ZOgEX.jpg"
         alt="trip_image"
         className="s-image"
       />
-      <div className="bottom">
-        <h2>the best trip</h2>
-      </div>
+      <div className="bottom"></div>
+      <h2>the best trip</h2>
     </div>
   );
 }
