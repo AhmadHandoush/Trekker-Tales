131f6c03fbebc9df451cb955bda93627c1bbd352
hamada <hamadahandoush9991@gmail.com>
2024-04-30 22:39:08 +0200

    Backend <> Add the accessing attribute

--- admin-dashboard/src/pages/main/components/TopCard.jsx
+++ admin-dashboard/src/pages/main/components/TopCard.jsx
@@ -1,12 +1,13 @@
 function TopCard({ trip }) {
-  const { name } = trip;
+  const { name, trip_image } = trip;
   return (
     <div className="top-card">
       <img
-        src="./360_F_113467839_JA7ZqfYTcIFQWAkwMf3mVmhqXr7ZOgEX.jpg"
-        alt="trip_image"
+        src={`http://yourdomain.com/storage/images/${trip_image}`}
+        alt={name}
         className="s-image"
       />
+
       <div className="bottom"></div>
       <h2>{name}</h2>
     </div>
