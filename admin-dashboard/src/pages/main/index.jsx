0a99e9c9149327014a1436a2fa3eed0fc7dc74a3
hamada <hamadahandoush9991@gmail.com>
2024-04-30 14:21:39 +0200

    Admin dashboard: create the trop rating trips section

--- admin-dashboard/src/pages/main/index.jsx
+++ admin-dashboard/src/pages/main/index.jsx
@@ -10,13 +10,17 @@ function Main() {
           <h1>13</h1>
         </InfoCard>
         <InfoCard>
-          <h2>Trips</h2>
+          <h2>Teachers</h2>
           <h1>13</h1>
         </InfoCard>
         <InfoCard>
-          <h2>Trips</h2>
+          <h2>Users</h2>
           <h1>13</h1>
         </InfoCard>
+      </section>
+      <section className="top-trips">
+        <h2>Highest Rating Trips:</h2>
+        <div className="top-trips-cards"></div>
       </section>
     </div>
   );
