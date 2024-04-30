01d617180e3e0e9c0603660dd288e3163a1a923c
hamada <hamadahandoush9991@gmail.com>
2024-04-30 20:34:37 +0200

    Backend <> create api to create a trip

--- server/app/Http/Controllers/TripController.php
+++ server/app/Http/Controllers/TripController.php
@@ -10,36 +10,68 @@ class TripController extends Controller
 {
     public function store(Request $request)
     {
-       $request->validate([
-            'name' => 'required|string',
-            'destination' => 'required|string',
-            'date' => 'required|date',
+    //    $request->validate([
+    //         'name' => 'required|string',
+    //         'destination' => 'required|string',
+    //         'date' => 'required|date',
+    //         'total_seats' => 'required|integer',
+    //         'available_seats' => 'required|integer',
+    //         'fees' => 'required|numeric',
+    //         'description' => 'nullable|string',
+    //         // 'trip_image' => 'nullable|string',
+    //         'locations' => 'required|array',
+    //         'locations.*' => 'exists:locations,id',
+    //     ]);
 
-            'total_seats' => 'required|integer',
-            'available_seats' => 'required|integer',
-            'fees' => 'required|numeric',
-            'description' => 'nullable|string',
-            'trip_image' => 'nullable|string',
-            'locations' => 'required|array',
-            'locations.*' => 'exists:locations,id',
-        ]);
+        // $trip = Trip::create([
+        //     'name' => $request->name,
+        //     'destination' => $request->destination,
+        //     'date' => $request->date,
+        //     'start_time' =>$request->start_time,
+        //     'end_time' =>$request->end_time,
+        //     'total_seats' => $request->total_seats,
+        //     'available_seats' => $request->available_seats,
+        //     'fees' => $request->fees,
+        //     'breakfast' => $request->breakfast,
+        //     'lunch' => $request->lunch,
+        //     'dinner' => $request->dinner,
+        //     'description' => $request->description,
+        //     'trip_image' => $request->trip_image
+        // ]);
 
 
-        $trip = Trip::create([
-            'name' => $request->name,
-            'destination' => $request->destination,
-            'date' => $request->date,
-            'start_time' =>$request->start_time,
-            'end_time' =>$request->end_time,
-            'total_seats' => $request->total_seats,
-            'available_seats' => $request->available_seats,
-            'fees' => $request->fees,
-            'breakfast' => $request->breakfast,
-            'lunch' => $request->lunch,
-            'dinner' => $request->dinner,
-            'description' => $request->description,
-            'trip_image' => $request->destination,
-        ]);
+
+
+        $trip = new Trip();
+        $trip->name = $request->name;
+        $trip->destination = $request->destination;
+        $trip->date = $request->date;
+        $trip->start_time = $request->start_time;
+        $trip->end_time = $request->end_time;
+        $trip->total_seats = $request->total_seats;
+        $trip->available_seats = $request->available_seats;
+        $trip->fees = $request->fees;
+        $trip->description = $request->description;
+
+        if ($request->hasFile('trip_image')) {
+            $image = $request->file('trip_image');
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
+            $image->storeAs('images', $imageName); // Store the image
+            $trip->trip_image = $imageName; // Save the image path to the database
+        }
+
+
+
+        // if ($request->hasFile('trip_image')) {
+        //     $image = $request->file('trip_image');
+        //     $imageName = time() . '.' . $image->getClientOriginalExtension();
+        //     $image->storeAs('images', $imageName); // Store the image
+        //     $trip->trip_image = $imageName; // Save the image path to the database
+        // }
+
+        $trip->save();
+
+
         $trip->locations()->attach($request->locations);
 
         return response()->json(['message' => 'Trip created successfully', 'trip' => $trip], 201);
@@ -67,23 +99,5 @@ class TripController extends Controller
 
         return response()->json(['trips_number' => $trips], 200);
     }
-
-
 }
-
-
-// 'name' => 'required|string',
-//             'destination' => 'required|string',
-//             'date' => 'required|date',
-//             'start_time' => 'required|date_format:H:i',
-//             'end_time' => 'required|date_format:H:i',
-//             'total_seats' => 'required|integer',
-//             'available_seats' => 'required|integer',
-//             'fees' => 'required|numeric',
-//             'breakfast' => 'boolean',
-//             'lunch' => 'boolean',
-//             'dinner' => 'boolean',
-//             'description' => 'nullable|string',
-//             'trip_image' => 'nullable|string',
-
 
