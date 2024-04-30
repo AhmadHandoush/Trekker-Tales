194f4f65069ddaa9f8ec4afc4094befef1cdc146
hamada <hamadahandoush9991@gmail.com>
2024-04-30 14:14:20 +0200

    Admin dashboard: create the main cards

---
+++ admin-dashboard/src/pages/main/components/InfoCard.jsx
@@ -0,0 +1,5 @@
+function InfoCard({ children }) {
+  return <div className="info-card">{children}</div>;
+}
+
+export default InfoCard;
--- admin-dashboard/src/pages/main/index.jsx
+++ admin-dashboard/src/pages/main/index.jsx
@@ -1,5 +1,25 @@
+import InfoCard from "./components/InfoCard";
+import "./main.css";
+
 function Main() {
-  return <div>MAin</div>;
+  return (
+    <div className="main">
+      <section className="cards-info flex">
+        <InfoCard>
+          <h2>Trips</h2>
+          <h1>13</h1>
+        </InfoCard>
+        <InfoCard>
+          <h2>Trips</h2>
+          <h1>13</h1>
+        </InfoCard>
+        <InfoCard>
+          <h2>Trips</h2>
+          <h1>13</h1>
+        </InfoCard>
+      </section>
+    </div>
+  );
 }
 
 export default Main;
