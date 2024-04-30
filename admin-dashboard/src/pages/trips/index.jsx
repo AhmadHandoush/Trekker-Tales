a18c698798bce8f33317c0be9217864f3744b8e8
hamada <hamadahandoush9991@gmail.com>
2024-04-30 22:42:32 +0200

    admin dashboard: create the trips page

--- admin-dashboard/src/pages/trips/index.jsx
+++ admin-dashboard/src/pages/trips/index.jsx
@@ -1,5 +1,15 @@
+import { IoMdAdd } from "react-icons/io";
 function Trips() {
-  return <div>trips</div>;
+  return (
+    <div className="trips">
+      <div className="add-trip">
+        <button>
+          Add
+          <IoMdAdd />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Trips;
