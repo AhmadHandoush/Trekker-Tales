900dae179928907e388daa4ee6bb341474b5a0dc
hamada <hamadahandoush9991@gmail.com>
2024-05-01 08:54:45 +0200

    admin dashboard: build the the structure of the page

--- admin-dashboard/src/pages/teachers/index.jsx
+++ admin-dashboard/src/pages/teachers/index.jsx
@@ -1,5 +1,90 @@
 function Teachers() {
-  return <div>teachers</div>;
+  return (
+    <div className="teachers">
+      <div className="titles">
+        <h3>Username</h3>
+        <h3>Email</h3>
+        <h3>Address</h3>
+        <button className="add-teacher">Add +</button>
+      </div>
+      <div className="second">
+        <ul className="teachers-list flex column">
+          <li className="user flex-between">
+            <div className="name">
+              <div className="image">
+                <img src="" alt="" className="s-image" />
+                <h2>Jhon Doe</h2>
+              </div>
+            </div>
+            <div className="email">
+              <h4>example@gmail.com</h4>
+            </div>
+            <div className="address">
+              <h4>Barsa Kura</h4>
+            </div>
+            <div className="update"></div>
+            <div className="delete">
+              <button>Delete</button>
+            </div>
+          </li>
+          <li className="user flex-between">
+            <div className="name">
+              <div className="image">
+                <img src="" alt="" className="s-image" />
+                <h2>Jhon Doe</h2>
+              </div>
+            </div>
+            <div className="email">
+              <h4>example@gmail.com</h4>
+            </div>
+            <div className="address">
+              <h4>Barsa Kura</h4>
+            </div>
+            <div className="update"></div>
+            <div className="delete">
+              <button>Delete</button>
+            </div>
+          </li>
+          <li className="user flex-between">
+            <div className="name">
+              <div className="image">
+                <img src="" alt="" className="s-image" />
+                <h2>Jhon Doe</h2>
+              </div>
+            </div>
+            <div className="email">
+              <h4>example@gmail.com</h4>
+            </div>
+            <div className="address">
+              <h4>Barsa Kura</h4>
+            </div>
+            <div className="update"></div>
+            <div className="delete">
+              <button>Delete</button>
+            </div>
+          </li>
+          <li className="user flex-between">
+            <div className="name">
+              <div className="image">
+                <img src="" alt="" className="s-image" />
+                <h2>Jhon Doe</h2>
+              </div>
+            </div>
+            <div className="email">
+              <h4>example@gmail.com</h4>
+            </div>
+            <div className="address">
+              <h4>Barsa Kura</h4>
+            </div>
+            <div className="update"></div>
+            <div className="delete">
+              <button>Delete</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Teachers;
