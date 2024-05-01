1786a280888afc78d3b52fe62b4559db9f5d9d06
hamada <hamadahandoush9991@gmail.com>
2024-05-01 08:57:14 +0200

    admin dashboard: add the list of users  structure

--- admin-dashboard/src/pages/teachers/index.jsx
+++ admin-dashboard/src/pages/teachers/index.jsx
@@ -27,60 +27,6 @@ function Teachers() {
               <button>Delete</button>
             </div>
           </li>
-          <li className="user flex-between">
-            <div className="name">
-              <div className="image">
-                <img src="" alt="" className="s-image" />
-                <h2>Jhon Doe</h2>
-              </div>
-            </div>
-            <div className="email">
-              <h4>example@gmail.com</h4>
-            </div>
-            <div className="address">
-              <h4>Barsa Kura</h4>
-            </div>
-            <div className="update"></div>
-            <div className="delete">
-              <button>Delete</button>
-            </div>
-          </li>
-          <li className="user flex-between">
-            <div className="name">
-              <div className="image">
-                <img src="" alt="" className="s-image" />
-                <h2>Jhon Doe</h2>
-              </div>
-            </div>
-            <div className="email">
-              <h4>example@gmail.com</h4>
-            </div>
-            <div className="address">
-              <h4>Barsa Kura</h4>
-            </div>
-            <div className="update"></div>
-            <div className="delete">
-              <button>Delete</button>
-            </div>
-          </li>
-          <li className="user flex-between">
-            <div className="name">
-              <div className="image">
-                <img src="" alt="" className="s-image" />
-                <h2>Jhon Doe</h2>
-              </div>
-            </div>
-            <div className="email">
-              <h4>example@gmail.com</h4>
-            </div>
-            <div className="address">
-              <h4>Barsa Kura</h4>
-            </div>
-            <div className="update"></div>
-            <div className="delete">
-              <button>Delete</button>
-            </div>
-          </li>
         </ul>
       </div>
     </div>
