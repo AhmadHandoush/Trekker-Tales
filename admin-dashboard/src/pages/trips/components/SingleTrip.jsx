50a4fd1aaefc052a70f10b43d305b582818d7fed
hamada <hamadahandoush9991@gmail.com>
2024-05-02 13:12:25 +0200

    Admin dashboard: design the right side of the single page

--- admin-dashboard/src/pages/trips/components/SingleTrip.jsx
+++ admin-dashboard/src/pages/trips/components/SingleTrip.jsx
@@ -50,14 +50,64 @@ function SingleTrip() {
   return (
     <div className="single ">
       <div className="info flex">
-        <div className="single-right">
+        <div className="single-left">
           <img
             src={`http://127.0.0.1:8000/${trip_image}`}
             alt={name}
             className="s-image"
           />
         </div>
-        <div className="single-left flex column"></div>
+        <div className="single-right flex column">
+          <h1>{name}</h1>
+          <div className="flex-items flex-between">
+            <h5>Destination:</h5>
+            <h4>{destination}</h4>
+          </div>{" "}
+          <div className="flex-items flex-between">
+            <h5>date:</h5>
+            <h4>{date}</h4>
+          </div>
+          <div className="flex-items flex-between">
+            <h5>Start_time:</h5>
+            <h4>{start_time}</h4>
+          </div>
+          <div className="flex-items flex-between">
+            <h5>End-time:</h5>
+            <h4>{end_time}</h4>
+          </div>
+          <div className="flex-items flex-between">
+            <h5>date:</h5>
+            <h4>{date}</h4>
+          </div>
+          <div className="flex-items flex-between">
+            <h5>Breakfast:</h5>
+            <h4>{breakfast}</h4>
+          </div>
+          <div className="flex-items flex-between">
+            <h5>Lunch:</h5>
+            <h4>{lunch}</h4>
+          </div>
+          <div className="flex-items flex-between">
+            <h5>Dinner:</h5>
+            <h4>{dinner}</h4>
+          </div>
+          <div className="flex-items flex-between">
+            <h5>Fee:</h5>
+            <h2>$ {parseInt(fees)}</h2>
+          </div>{" "}
+          <div className="flex-items flex-between">
+            <h5>Locations:</h5>
+            <h2>
+              {locations.map((location) => (
+                <span>{location.name} </span>
+              ))}
+            </h2>
+          </div>
+          <div className="flex-items flex-between">
+            <h5>Description:</h5>
+            <h2>{description}</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
