c4c2b8300129f74916e85679c367883036e912c5
hamada <hamadahandoush9991@gmail.com>
2024-05-04 23:46:58 +0200

    Mobile App <> create the home page

--- trekker-tales/app/(tabs)/home/index.jsx
+++ trekker-tales/app/(tabs)/home/index.jsx
@@ -3,12 +3,36 @@ import React from "react";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.home}>
+      <View style={styles.top}>
+        <Text style={styles.hi}>Hi Ahmad </Text>
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  home: {
+    backgroundColor: "white",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    flex: 1,
+  },
+  top: {
+    height: "25%",
+    width: "100%",
+    backgroundColor: "#E87A00",
+    borderBottomEndRadius: 30,
+    borderBottomStartRadius: 30,
+  },
+  hi: {
+    marginTop: 50,
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 24,
+    marginLeft: 20,
+  },
+});
