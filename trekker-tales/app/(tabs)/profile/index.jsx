7bc27031692e4f3df3586d090d57bf453a48e7be
hamada <hamadahandoush9991@gmail.com>
2024-05-05 14:50:05 +0200

    Mobile App <> create the box for the user photo

--- trekker-tales/app/(tabs)/profile/index.jsx
+++ trekker-tales/app/(tabs)/profile/index.jsx
@@ -1,10 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 
 const Profile = () => {
+  // const [user, setUser] = useState([]);
+  const user = {
+    name: "Ahmad Handoush",
+    email: "ahmadhandoush5@gmail.com",
+    phone: "81303288",
+    address: "Koura",
+    profile_image:
+      "../../../assets/360_F_302884605_actpipOdPOQHDTnFtp4zg4RtlWzhOASp.jpg",
+  };
+  const { name, email, phone, address, profile_image } = user;
   return (
     <View style={styles.profile}>
-      <View style={styles.top}></View>
+      <View style={styles.top}>
+        <View style={styles.image}>
+          <Image source={profile_image} style={styles.img}></Image>
+        </View>
+      </View>
     </View>
   );
 };
@@ -24,5 +38,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#E87A00",
     borderBottomEndRadius: 30,
     borderBottomStartRadius: 30,
+    position: "relative",
+  },
+  image: {
+    position: "absolute",
+    bottom: "-30%",
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    overflow: "hidden",
+    left: "25%",
+    backgroundColor: "#d7d7d7",
+    transform: [{ translateX: 50 }],
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+    maxHeight: "100%",
+    maxWidth: "100%",
   },
 });
