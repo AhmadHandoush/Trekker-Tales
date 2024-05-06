247c406b4a3c8c7f279d7f76a4f8651293823b9c
hamada <hamadahandoush9991@gmail.com>
2024-05-06 21:02:43 +0200

    Admin dashboard : Add the loader to the single trip page

--- admin-dashboard/src/pages/trips/components/SingleTrip.jsx
+++ admin-dashboard/src/pages/trips/components/SingleTrip.jsx
@@ -49,60 +49,64 @@ function SingleTrip() {
   }, []);
   return (
     <div className="single ">
-      <div className="info flex">
-        <div className="single-left">
-          <img
-            src={`http://127.0.0.1:8000/${trip_image}`}
-            alt={name}
-            className="s-image"
-          />
-        </div>
-        <div className="single-right flex column">
-          <h1>{name}</h1>
-          <div className="flex-items flex-between">
-            <h5>Destination:</h5>
-            <h4>{destination}</h4>
-          </div>{" "}
-          <div className="flex-items flex-between">
-            <h5>date:</h5>
-            <h4>{date}</h4>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="info flex">
+          <div className="single-left">
+            <img
+              src={`http://127.0.0.1:8000/${trip_image}`}
+              alt={name}
+              className="s-image"
+            />
           </div>
-          <div className="flex-items flex-between">
-            <h5>Start_time:</h5>
-            <h4>{start_time}</h4>
-          </div>
-          <div className="flex-items flex-between">
-            <h5>End-time:</h5>
-            <h4>{end_time}</h4>
-          </div>
-          <div className="flex-items flex-between">
-            <h5>date:</h5>
-            <h4>{date}</h4>
-          </div>
-          <div className="flex-items flex-between">
-            <h5>Fee:</h5>
-            <h2>$ {parseInt(fees)}</h2>
-          </div>{" "}
-          <div className="flex-items flex-between">
-            <h5>Locations:</h5>
-            <h2>
+          <div className="single-right flex column">
+            <h1>{name}</h1>
+            <div className="flex-items flex-between">
+              <h5>Destination:</h5>
+              <h4>{destination}</h4>
+            </div>{" "}
+            <div className="flex-items flex-between">
+              <h5>date:</h5>
+              <h4>{date}</h4>
+            </div>
+            <div className="flex-items flex-between">
+              <h5>Start_time:</h5>
+              <h4>{start_time}</h4>
+            </div>
+            <div className="flex-items flex-between">
+              <h5>End-time:</h5>
+              <h4>{end_time}</h4>
+            </div>
+            <div className="flex-items flex-between">
+              <h5>date:</h5>
+              <h4>{date}</h4>
+            </div>
+            <div className="flex-items flex-between">
+              <h5>Fee:</h5>
+              <h2>$ {parseInt(fees)}</h2>
+            </div>{" "}
+            <div className="flex-items flex-between">
+              <h5>Locations:</h5>
+              {/* <h2>
               {setOk &&
                 locations.map((location) => <span>{location.name} </span>)}
-            </h2>
-          </div>
-          <div className="flex-items flex-between">
-            <h5>Description:</h5>
-            <p>{description}</p>
-          </div>
-          <div className="flex-items flex-between">
-            <h5>Status:</h5>
-            <h2>{status}</h2>
-          </div>
-          <div className="update-trip flex-center">
-            <button className="btn-update">Update</button>
+            </h2> */}
+            </div>
+            <div className="flex-items flex-between">
+              <h5>Description:</h5>
+              <p>{description}</p>
+            </div>
+            <div className="flex-items flex-between">
+              <h5>Status:</h5>
+              <h2>{status}</h2>
+            </div>
+            <div className="update-trip flex-center">
+              <button className="btn-update">Update</button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
