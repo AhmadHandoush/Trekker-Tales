ea5820cb294c0969ff902e153f6395f0d5efed4b
hamada <hamadahandoush9991@gmail.com>
2024-05-06 15:55:19 +0200

    Mobile App <> add some style to the booking box

--- trekker-tales/Components/bookcard.jsx
+++ trekker-tales/Components/bookcard.jsx
@@ -2,10 +2,32 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const BookCard = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("http:://00000", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data: inputValue }),
+      });
+
+      if (response.ok) {
+        Alert.alert("Success", "booking done");
+      } else {
+        Alert.alert("Error", "Failed to send data");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      Alert.alert("Error", "Failed to send data");
+    }
+  };
   return (
     <View style={styles.box}>
       <TextInput
-        placeholder="Enter data"
+        placeholder="Please enter your child's name"
         onChangeText={setInputValue}
         value={inputValue}
       />
@@ -16,4 +38,17 @@ const BookCard = () => {
 
 export default BookCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  box: {
+    position: "absolute",
+    zIndex: 20000,
+    width: "80%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    backgroundColor: "white",
+    borderRadius: 6,
+    padding: 20,
+    top: "50%",
+    left: "50%",
+  },
+});
