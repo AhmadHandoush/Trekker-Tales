1aa0a63cfd19c27e2d98269fdf11f2a11fa82267
hamada <hamadahandoush9991@gmail.com>
2024-05-07 10:34:32 +0200

    Mobile App:create the top section of the post

--- trekker-tales/Components/post.jsx
+++ trekker-tales/Components/post.jsx
@@ -3,12 +3,22 @@ import React from "react";
 
 const Post = () => {
   return (
-    <View>
-      <Text>Post</Text>
+    <View style={styles.post}>
+      <View style={styles.top}>
+        <View>
+          <Image />
+        </View>
+        <View>
+          <Text>Ahmad </Text>
+          <Text>minutes</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default Post;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  top: {},
+});
