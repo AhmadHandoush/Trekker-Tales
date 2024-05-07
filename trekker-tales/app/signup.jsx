5188dcef2c105755f9a04527b744776e19c3ff31
hamada <hamadahandoush9991@gmail.com>
2024-05-07 13:33:06 +0200

    Mobile App:Link the Sign Up page

--- trekker-tales/app/signup.jsx
+++ trekker-tales/app/signup.jsx
@@ -1,5 +1,13 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 import { Link } from "expo-router";
 import Topline from "../Components/topline";
 import Input from "../Components/input";
@@ -7,6 +15,41 @@ import Button from "../Components/button";
 import OrWith from "../Components/orwith";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSignup = () => {
+    const userData = {
+      name: name,
+      email: email,
+      password: password,
+    };
+
+    fetch("http://127.0.0.1:8000/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Signup failed");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Alert.alert("Signup Successful", "You have successfully signed up.");
+        setSuccess(true);
+      })
+      .catch((error) => {
+        Alert.alert("Signup Error", error.message);
+        setError(error);
+      });
+  };
   return (
     <View style={styles.container}>
       <View style={styles.logo}>
@@ -18,11 +61,47 @@ const Signup = () => {
       <View style={styles.login}>
         <Topline />
         <Text style={styles.title}>Sign Up</Text>
+        <View
+          style={{ marginLeft: "auto", marginRight: "auto", marginBottom: 10 }}
+        >
+          <Text style={styles.label}>Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your name"
+            onChangeText={setName}
+            value={name}
+          />
+        </View>
+        <View
+          style={{ marginLeft: "auto", marginRight: "auto", marginBottom: 10 }}
+        >
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your email"
+            onChangeText={setEmail}
+            value={email}
+            keyboardType="email-address"
+          />
+        </View>
+        <View
+          style={{ marginLeft: "auto", marginRight: "auto", marginBottom: 10 }}
+        >
+          <Text style={styles.label}>password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your password"
+            onChangeText={setPassword}
+            value={password}
+            secureTextEntry={true}
+          />
+        </View>
 
-        <Input label={"Name"} placerholder={"Enter your name"} />
-        <Input label={"Email"} placerholder={"Enter your email"} />
-        <Input label={"Password"} placerholder={"Enter your password"} />
-        <Button title="Sign Up" />
+        {/* <Button title="Sign Up" onPress={handleSignup} /> */}
+        <TouchableOpacity style={styles.button} onPress={handleSignup}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+
         <Text style={styles.passText}>
           Already have an account?{" "}
           <Link href={"/login"}>
@@ -32,6 +111,9 @@ const Signup = () => {
             </Text>
           </Link>
         </Text>
+        {success && (
+          <Text style={styles.success}>Account creaated Successfully...</Text>
+        )}
         <Link href={"/(tabs)/home"}>
           <Text style={{ color: "#E87A00", fontWeight: "bold" }}> Home </Text>
         </Link>
@@ -50,6 +132,7 @@ const styles = StyleSheet.create({
 
     flex: 1,
   },
+
   logo: {
     display: "flex",
     justifyContent: "center",
@@ -63,6 +146,10 @@ const styles = StyleSheet.create({
     borderTopEndRadius: 30,
     borderTopStartRadius: 30,
   },
+  success: {
+    color: "green",
+    marginLeft: 30,
+  },
   title: {
     textAlign: "center",
     marginTop: 20,
@@ -75,5 +162,38 @@ const styles = StyleSheet.create({
     color: "black",
     padding: 10,
     marginLeft: 22,
+  },
+  input: {
+    height: 45,
+    borderWidth: 0,
+    borderRadius: 6,
+    width: 325,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#FFE1E4",
+    backgroundColor: "white",
+  },
+  label: {
+    paddingVertical: "1%",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  button: {
+    width: 325,
+    height: 48,
+    borderRadius: 10,
+    backgroundColor: "#E87A00",
+    marginRight: "auto",
+    marginLeft: "auto",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    paddingTop: "4%",
+    alignSelf: "center",
+
+    verticalAlign: "top",
   },
 });
