3dc06be4e886a588b4b2a37988b21020845b8fe1
hamada <hamadahandoush9991@gmail.com>
2024-05-08 11:17:05 +0200

    Mobile App <> create the receivedmessage page

---
+++ trekker-tales/Components/receivedMessage.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ReceivedMessage = () => {
+  return (
+    <View>
+      <Text>ReceivedMessage</Text>
+    </View>
+  );
+};
+
+export default ReceivedMessage;
+
+const styles = StyleSheet.create({});
--- trekker-tales/app/(tabs)/more/chats.jsx
+++ trekker-tales/app/(tabs)/more/chats.jsx
@@ -2,11 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const Chats = () => {
-  return (
-    <View>
-      <Text>Chats</Text>
-    </View>
-  );
+  return <View></View>;
 };
 
 export default Chats;
