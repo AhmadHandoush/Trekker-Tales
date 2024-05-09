c71996a71990a43f882b336a0bda7fe47b87b768
hamada <hamadahandoush9991@gmail.com>
2024-05-09 21:23:39 +0200

    Mobile App <> create the highest trip page

---
+++ trekker-tales/app/(tabs)/home/highest.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const highest = () => {
+  return (
+    <View>
+      <Text>highest</Text>
+    </View>
+  );
+};
+
+export default highest;
+
+const styles = StyleSheet.create({});
