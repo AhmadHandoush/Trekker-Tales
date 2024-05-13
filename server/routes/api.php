ae1f3721ab586547ec05a0b779f3bbf66312b307
hamada <hamadahandoush9991@gmail.com>
2024-05-13 10:47:23 +0200

    Mobile App <> get all the parents

--- server/routes/api.php
+++ server/routes/api.php
@@ -108,6 +108,7 @@ Route::group(['middleware','isTeacher'],function (){
         Route::get('get_children/{trip_id}',[BookController::class,'get_children']);
         Route::get('get_trips',[TripController::class,'get_trips']);
         Route::get('get_trip/{trip_id}',[TripController::class,'get_trip']);
+        Route::get('get_parents',[UserController::class,'get_parents']);
 
 
 
--- trekker-tales/app/(teacher)/chats/index.jsx
+++ trekker-tales/app/(teacher)/chats/index.jsx
@@ -6,13 +6,50 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatCard from "../../../Components/chatone";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { BASE_URL } from "../../utils/constants";
 
 const Chats = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchTrips = async () => {
+      setLoading(true);
+      try {
+        const token = await AsyncStorage.getItem("token");
+        if (token) {
+          const response = await fetch(`${BASE_URL}/api/get_parents`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          const data = await response.json();
+
+          setUsers(data.parents);
+          setLoading(false);
+        } else {
+          setLoading(true);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchTrips();
+  }, []);
   return (
-    <View style={styles.chats}>
-      <ChatCard />
+    <View>
+      {users && (
+        <View style={styles.chats}>
+          {users.map((user) => (
+            <ChatCard user={user} key={user.id} />
+          ))}
+        </View>
+      )}
     </View>
   );
 };
@@ -24,5 +61,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 120,
     padding: 10,
+    gap: 10,
   },
 });
