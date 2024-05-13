be0294fba42b320cb7d2bcc378028a9621ac933c
hamada <hamadahandoush9991@gmail.com>
2024-05-13 09:58:59 +0200

    Mobile App <> create the card structure

--- trekker-tales/app/(teacher)/chats/index.jsx
+++ trekker-tales/app/(teacher)/chats/index.jsx
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ImageBase,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 
 const Chats = () => {
   return (
-    <View>
-      <Text>Chats</Text>
-    </View>
+    <TouchableOpacity>
+      <View>
+        <View style={styles.card}>
+          <Image
+            source={require("../../../assets/360_F_113467839_JA7ZqfYTcIFQWAkwMf3mVmhqXr7ZOgEX.jpg")}
+            style={styles.image}
+          />
+          <Text style={styles.name}>Name</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
